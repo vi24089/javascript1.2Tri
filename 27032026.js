18d8833cb184665a168ad678d4166b89c8fda306
vi24089 <vitoria.magalhaes.costa@escola.pr.gov.br>
2026-03-27 07:54:46 -0300

    update

---
+++ 27032026.js
@@ -0,0 +1,21 @@
+// Criar Array 
+
+const fila = [1,2,3,4,5,6];
+
+const filasStringAmigos = ["Vitória","Isadora","Estevam"]
+
+function lerAmigos(filaStringsAmigos){
+     console.log(`Você tem ${filaStringsAmigos.length} amigos`)
+
+     for(let i = 0; i < filaStringsAmigos.length;i++){
+         console.log(filaStringsAmigos[i])
+         if(filaStringsAmigos[i]=== "Vitória"){
+            console.log(`Index do seu melhor amigo é ${i}`)
+
+     }
+
+    
+}
+
+
+
